8b6a718bee1a23e5fedf831485f06a464274eaf2
iqbaalr <136084361+iqbaalr@users.noreply.github.com>
2023-07-01 23:57:39 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,110 +1,179 @@
-// Mengambil data dari local storage (jika ada)
-const akanDilakukan = JSON.parse(localStorage.getItem('akanDilakukan')) || [];
-const sudahDilakukan = JSON.parse(localStorage.getItem('sudahDilakukan')) || [];
+// Mendapatkan referensi ke elemen <tbody>, input pencarian, dan tombol pencarian
+var index = document.querySelector('#index');
+var searchInput = document.querySelector('#searchInput');
+var searchButton = document.querySelector('#searchButton');
 
-// Fungsi untuk menyimpan data ke local storage
-function saveTasks() {
-    localStorage.setItem('akanDilakukan', JSON.stringify(akanDilakukan));
-    localStorage.setItem('sudahDilakukan', JSON.stringify(sudahDilakukan));
-}
-// Memuat daftar task yang akan dilakukan dan sudah dilakukan saat memulai aplikasi
-loadTasks();
+// Fungsi untuk memuat daftar film berdasarkan judul pencarian
+function loadMoviesByTitle(title) {
+    // Membersihkan isi tabel sebelum memuat data baru
+    index.innerHTML = '';
 
-// Fungsi untuk menambahkan task baru
-function addTask() {
-    var taskInput = document.getElementById('taskInput');
-    var taskTgl = document.getElementById('taskk');
-    var taskText = taskInput.value.trim();
-    var taskDate = taskTgl.value.trim();
-    if (taskText !== '' & taskDate !== '') {
-        var task = {
-            text: taskText,
-            date: taskDate
-        };
-        akanDilakukan.push(task);
-        saveTasks();
-        taskInput.value = '';
-        loadTasks();
-    }
-    else {
-        alert("Mohon Masukkan Input teks dan Input tanggal yang benar!");
-    }
-}
+    // Mengambil data dari API TMDb berdasarkan judul pencarian
+    fetch('https://api.themoviedb.org/3/search/movie?api_key=8786ffd1b392111a67759e0e7389bc3a&query=' + encodeURIComponent(title))
+        .then(response => response.json())
+        .then(data => {
+            const movieSearching = document.querySelector('.movie-searching');
 
-// Fungsi untuk menyelesaikan suatu task
-function completeTask(index) {
-    var task = akanDilakukan.splice(index, 1)[0];
-    sudahDilakukan.push(task);
-    saveTasks();
-    loadTasks();
-}
+            // Menampilkan daftar film
+            data.results.forEach(movie => {
+                const movieCard = document.createElement('div');
+                movieCard.classList.add('movie-card');
 
-// Fungsi untuk mengembalikan task yang sudah sudahDilakukan ke daftar "Yang akan dilakukan"
-function restoreTask(index) {
-    var task = sudahDilakukan.splice(index, 1)[0];
-    akanDilakukan.push(task);
-    saveTasks();
-    loadTasks();
-}
+                const movieImage = document.createElement('img');
+                movieImage.src = 'https://image.tmdb.org/t/p/w200' + movie.poster_path;
+                movieImage.alt = movie.title;
 
-// Fungsi untuk menghapus task yang sudah sudahDilakukan
-function deleteTask(index) {
-    sudahDilakukan.splice(index, 1);
-    saveTasks();
-    loadTasks();
+                const movieTitle = document.createElement('h2');
+                movieTitle.textContent = movie.title;
+
+                const movieRating = document.createElement('p');
+                movieRating.textContent = 'Rating: ' + movie.vote_average + " \u{2B50}";
+                const moviePopularity = document.createElement('p');
+                moviePopularity.textContent = 'Popularity: ' + movie.popularity;
+
+                movieCard.appendChild(movieImage);
+                movieCard.appendChild(movieTitle);
+                movieCard.appendChild(movieRating);
+                movieCard.appendChild(moviePopularity);
+
+                // Menambahkan event listener pada poster
+                movieImage.addEventListener('click', function () {
+                    showMovieDetails(movie);
+                });
+
+                movieSearching.appendChild(movieCard);
+            });
+        })
+        .catch(error => {
+            console.error('Terjadi kesalahan:', error);
+        });
 }
 
-// Fungsi untuk memuat ulang daftar task
-function loadTasks() {
-    var todoList = document.getElementById('todoList');
-    var doneList = document.getElementById('doneList');
+// Event listener untuk tombol pencarian
+searchButton.addEventListener('click', function () {
+    var searchTitle = searchInput.value;
+    loadMoviesByTitle(searchTitle);
+});
+// Mengambil data dari API TMDb
+fetch('https://api.themoviedb.org/3/trending/movie/week?api_key=8786ffd1b392111a67759e0e7389bc3a')
+    .then(response => response.json())
+    .then(data => {
+        const movieContainer = document.querySelector('.movie-container');
 
-    // Mengosongkan daftar task sebelum memuat yang baru
-    todoList.innerHTML = '';
-    doneList.innerHTML = '';
+        // Menampilkan daftar film
+        data.results.forEach(movie => {
+            const movieCard = document.createElement('div');
+            movieCard.classList.add('movie-card');
 
-    // Memuat task yang akan dilakukan
-    for (var i = 0; i < akanDilakukan.length; i++) {
-        var task = akanDilakukan[i];
-        var listItem = createTaskListItem(task.text, task.date, i, false);
-        todoList.appendChild(listItem);
-    }
+            const movieImage = document.createElement('img');
+            movieImage.src = 'https://image.tmdb.org/t/p/w500' + movie.poster_path;
+            movieImage.alt = movie.title;
 
-    // Memuat task yang sudah dilakukan
-    for (var j = 0; j < sudahDilakukan.length; j++) {
-        var doneTask = sudahDilakukan[j];
-        var doneListItem = createTaskListItem(doneTask.text, doneTask.date, j, true);
-        doneList.appendChild(doneListItem);
-    }
-}
+            const movieTitle = document.createElement('h2');
+            movieTitle.textContent = movie.title;
 
-// Fungsi untuk membuat elemen daftar task
-function createTaskListItem(text, date, index, isDone) {
-    var listItem = document.createElement('li');
-    var checkbox = document.createElement('input');
-    checkbox.type = 'checkbox';
-    checkbox.checked = isDone;
-    checkbox.addEventListener('change', function () {
-        if (this.checked) {
-            completeTask(index);
-        } else {
-            restoreTask(index);
+            const movieRating = document.createElement('p');
+            movieRating.textContent = 'Rating: ' + movie.vote_average + " \u{2B50}";
+            const moviePopularity = document.createElement('p');
+            moviePopularity.textContent = 'Popularity: ' + movie.popularity;
+
+            movieCard.appendChild(movieImage);
+            movieCard.appendChild(movieTitle);
+            movieCard.appendChild(movieRating);
+            movieCard.appendChild(moviePopularity)
+
+            // Menambahkan event listener pada poster
+            movieImage.addEventListener('click', function () {
+                showMovieDetails(movie);
+            });
+
+            movieContainer.appendChild(movieCard);
+        });
+    })
+    .catch(error => {
+        console.error('Terjadi kesalahan:', error);
+    });
+
+// Mendapatkan daftar TV shows
+fetch('https://api.themoviedb.org/3/trending/tv/week?api_key=8786ffd1b392111a67759e0e7389bc3a')
+    .then(response => response.json())
+    .then(data => {
+        const movieContainer = document.querySelector('.movie-tv');
+
+        // Menampilkan daftar TV shows
+        data.results.forEach(show => {
+            const showCard = document.createElement('div');
+            showCard.classList.add('movie-card');
+
+            const showImage = document.createElement('img');
+            showImage.src = 'https://image.tmdb.org/t/p/w500' + show.poster_path;
+            showImage.alt = show.name;
+
+            const showTitle = document.createElement('h2');
+            showTitle.textContent = show.name;
+            const showRating = document.createElement('p');
+            showRating.textContent = 'Rating: ' + show.vote_average + " \u{2B50}";
+            const moviePopularity = document.createElement('p');
+            moviePopularity.textContent = 'Popularity: ' + show.popularity;
+
+            showCard.appendChild(showImage);
+            showCard.appendChild(showTitle);
+            showCard.appendChild(showRating);
+            showCard.appendChild(moviePopularity);
+
+            // Menambahkan event listener pada poster
+            showImage.addEventListener('click', function () {
+                showMovieDetails(show);
+            });
+
+            movieContainer.appendChild(showCard);
+        });
+    })
+    .catch(error => {
+        console.error('Terjadi kesalahan:', error);
+    });
+
+
+// Fungsi untuk menampilkan informasi film saat poster di tekan
+function showMovieDetails(movie) {
+    // Membuat elemen <div> untuk overlay
+    const overlay = document.createElement('div');
+    overlay.classList.add('overlay');
+
+    // Membuat elemen <div> untuk informasi film
+    const movieInfo = document.createElement('div');
+    movieInfo.classList.add('movie-info');
+    movieInfo.style.color = '#000000';
+    movieInfo.style.borderRadius = '10%';
+
+    // Membuat elemen <h2> untuk judul film
+    const title = document.createElement('h2');
+    title.textContent = movie.title;
+    title.style.color = '#000000'
+
+    const showTitle = document.createElement('h2');
+    showTitle.textContent = movie.name;
+
+    // Membuat elemen <p> untuk deskripsi film
+    const overview = document.createElement('p');
+    overview.textContent = movie.overview;
+
+    // Menambahkan elemen-elemen ke dalam movieInfo
+    movieInfo.appendChild(showTitle);
+    movieInfo.appendChild(title);
+    movieInfo.appendChild(overview);
+
+    // Menambahkan movieInfo ke dalam overlay
+    overlay.appendChild(movieInfo);
+
+    // Menambahkan overlay ke dalam body
+    document.body.appendChild(overlay);
+
+    // Event listener untuk menutup overlay saat diklik di luar informasi film
+    overlay.addEventListener('click', function (event) {
+        if (event.target === overlay) {
+            overlay.remove();
         }
     });
-    listItem.appendChild(checkbox);
-
-    var taskText = document.createElement('span');
-    taskText.textContent = '' + date + ' : ' + text;
-    listItem.appendChild(taskText);
-
-    if (isDone) {
-        var deleteButton = document.createElement('button');
-        deleteButton.textContent = 'Hapus';
-        deleteButton.addEventListener('click', function () {
-            deleteTask(index);
-        });
-        listItem.appendChild(deleteButton);
-    }
-
-    return listItem;
 }
+
